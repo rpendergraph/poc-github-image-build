d6b6ceaee0f7e094957cdc7b8f347e488c5d5f16
Raymond Pendergraph <rpendergraph@arkbluecross.com>
2023-03-28 12:14:00 -0400

    Minimum working version

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import os
 import yaml
-from azure.keyvault.secrets import SecretClient
+from azure.keyvault.keys import KeyClient
 from azure.identity import ClientSecretCredential
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def main():
-    vault_url = os.environ["VAULT_URL"]
-    print("accessing vault at {}".format(vault_url))
     credential = ClientSecretCredential(
         client_id=os.environ["AZURE_CLIENT_ID"],
         client_secret=os.environ["AZURE_CLIENT_SECRET"],
         tenant_id=os.environ["AZURE_TENANT_ID"],
-        connection_verify=False
+        # connection_verify=False
     )
-    client = SecretClient(vault_url=vault_url, credential=credential)
-    key= client.get_secret(os.environ["PRIVATE_KEY_KEY"]).value    
-    print("the private key is {}".format(key))
+    vault_url = os.environ["VAULT_URL"]
+    print("accessing vault at {}".format(vault_url))
+    client = KeyClient(vault_url=vault_url, credential=credential)
+    key = client.get_key("my-private-key").key
+    print("the private key ID is {}".format(key.kid))
 
 if __name__ == "__main__":
     main()
